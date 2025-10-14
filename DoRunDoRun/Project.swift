12fa17e9f034453566b293e90506d7de53fa4668
Jaehui Yu <pkj0522@naver.com>
2025-10-14 15:59:37 +0900

    chore: NaverMaps 사용을 위한 Info.plist 설정 및 위치 권한 문구 추가

--- DoRunDoRun/Project.swift
+++ DoRunDoRun/Project.swift
@@ -19,6 +19,10 @@ let project = Project(
                         "UIInterfaceOrientationPortrait"
                     ],
                     "UIUserInterfaceStyle": "Light",
+                    "NSLocationWhenInUseUsageDescription": "러닝 중 현재 위치와 이동 경로를 지도에 표시하고, 달린 거리를 계산하기 위해 위치 정보를 사용합니다.",
+                    "NSLocationAlwaysAndWhenInUseUsageDescription": "앱이 백그라운드에 있을 때도 러닝 거리와 경로를 정확히 기록하기 위해 위치 정보를 사용합니다.",
+                    "UIBackgroundModes": ["location"],
+                    "NMFNcpKeyId": "$(NMFNcpKeyId)",
                 ]
             ),
             buildableFolders: [
@@ -30,6 +34,13 @@ let project = Project(
                 .external(name: "NMapsMap"),
                 .external(name: "Alamofire"),
             ],
+            settings: .settings(
+                base: [:],
+                configurations: [
+                    .debug(name: "Debug", xcconfig: "../Configs/Debug.xcconfig"),
+                    .release(name: "Release", xcconfig: "../Configs/Release.xcconfig"),
+                ]
+            ),
         )
     ]
 )
