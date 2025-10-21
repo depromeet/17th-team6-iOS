34bf1297d65a094ed327cbf2b50cd40204c77e1f
zaehorang <jaho001@uos.ac.kr>
2025-10-21 17:50:56 +0900

    feat: Running 관련 entity 추가

---
+++ DoRunDoRun/Sources/Domain/Entity/Running/RunningMetrics.swift
@@ -0,0 +1,20 @@
+//
+//  RunningMetrics.swift
+//  DoRunDoRun
+//
+//  Created by zaehorang on 10/21/25.
+//
+
+import Foundation
+
+/// 런닝 누적 지표
+struct RunningMetrics {
+    /// 누적 이동거리 (m)
+    let totalDistanceMeters: Double
+    /// 누적 경과시간 (일시정지 제외)
+    let elapsed: Duration
+    /// 평균 페이스 (초/킬로미터)
+    let avgPaceSecPerKm: Double
+    /// 현재 케이던스 (steps/min)
+    let cadenceSpm: Double
+}
