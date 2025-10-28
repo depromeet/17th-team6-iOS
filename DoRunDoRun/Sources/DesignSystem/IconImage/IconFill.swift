a3dfe83cf91dc2cf5525edf1d3789a6eb98cac30
Jaehui Yu <pkj0522@naver.com>
2025-10-28 14:17:51 +0900

    feat: 아이콘 에셋 추가 및 IconStyle · IconFill · IconSize 시스템 구축

---
+++ DoRunDoRun/Sources/DesignSystem/IconImage/IconFill.swift
@@ -0,0 +1,11 @@
+//
+//  IconFill.swift
+//  DoRunDoRun
+//
+//  Created by Jaehui Yu on 10/28/25.
+//
+
+enum IconFill: String {
+    case normal = ""          // 기본 아이콘
+    case fill = "_fill"       // 채워진 아이콘
+}
---
+++ DoRunDoRun/Sources/DesignSystem/IconImage/IconStyle.swift
@@ -0,0 +1,87 @@
+//
+//  IconStyle.swift
+//  DoRunDoRun
+//
+//  Created by Jaehui Yu on 10/28/25.
+//
+
+enum IconStyle: String {
+    // MARK: - Arrow
+    case arrowUp = "ic_arrow_up"
+    case arrowDown = "ic_arrow_down"
+    case arrowLeft = "ic_arrow_left"
+    case arrowRight = "ic_arrow_right"
+    
+    // MARK: - Check
+    case check = "ic_check"
+    case checkCircle = "ic_check_circle"
+    
+    // MARK: - Close
+    case close = "ic_close"
+    case closeCircle = "ic_close_circle"
+    
+    // MARK: - Info
+    case info = "ic_info"
+    case infoCircle = "ic_info_circle"
+    
+    // MARK: - Question
+    case questionCircle = "ic_question_circle"
+    
+    // MARK: - Alarm
+    case alarm = "ic_alarm"
+    case alarmActive = "ic_alarm_active"
+    
+    // MARK: - Edit
+    case edit = "ic_edit"
+    
+    // MARK: - Play
+    case play = "ic_play"
+    
+    // MARK: - Pause
+    case pause = "ic_pause"
+    
+    // MARK: - Gps
+    case gps = "ic_gps"
+    
+    // MARK: - More
+    case more = "ic_more"
+    
+    // MARK: - Running
+    case running = "ic_running"
+    
+    // MARK: - Feed
+    case feed = "ic_feed"
+    
+    // MARK: - Profile
+    case profile = "ic_profile"
+    
+    // MARK: - Distance
+    case distance = "ic_distance"
+    
+    // MARK: - Time
+    case time = "ic_time"
+    
+    // MARK: - Pace
+    case pace = "ic_pace"
+    
+    // MARK: - Firends
+    case firends = "ic_firends"
+    
+    // MARK: - Emoji
+    case emoji = "ic_emoji"
+    
+    // MARK: - React
+    case react = "ic_react"
+    
+    // MARK: - Setting
+    case setting = "ic_setting"
+    
+    // MARK: - Download
+    case download = "ic_download"
+    
+    // MARK: - Camera
+    case camera = "ic_camera"
+    
+    // MARK: - Add
+    case add = "ic_add"
+}
