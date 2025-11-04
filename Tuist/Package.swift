8a7fcca9b573f38d7d4a18d162c1276bcf558f10
Jaehui Yu <pkj0522@naver.com>
2025-11-04 17:23:24 +0900

    fix: Xcode 26에서 SPM "The data couldn’t be read" 오류 해결 및 Tuist 의존성 안정화
    - Firebase XCFramework 누락 문제 해결 (productTypes: .framework 강제)
    - .external(name:) → .package(product:) 전면 교체 (Tuist 4.x+ 및 Xcode 26 SPM 통합 호환)
    - Project.swift에 packages: [...] 명시적 정의 추가
    - FirebaseAnalytics, FirebaseCore, FirebaseMessaging 동적 링크 설정
    - Firebase 11.15.0 버전 고정 및 초기화 성공 (FCM 토큰 발급 확인)
    - buildableFolders → sources/resources 교체
    - OTHER_LDFLAGS 배열 형식 통일

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -4,12 +4,20 @@ import PackageDescription
 #if TUIST
     import struct ProjectDescription.PackageSettings
 
-    let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
-        productTypes: [:]
-    )
+let packageSettings = PackageSettings(
+    // productType을 지정해 Tuist가 framework로 링크하도록 명시
+    productTypes: [
+        "ComposableArchitecture": .framework,
+        "NMapsMap": .framework,
+        "Alamofire": .framework,
+        "Moya": .framework,
+        "Kingfisher": .framework,
+        "FirebaseAnalytics": .framework,
+        "FirebaseCore": .framework,
+        "FirebaseMessaging": .framework,
+        "FirebaseCoreDiagnostics": .framework
+    ]
+)
 #endif
 
 let package = Package(
@@ -20,6 +28,6 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.8.0"),
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.6.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.5.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "11.5.0")),
     ]
 )
