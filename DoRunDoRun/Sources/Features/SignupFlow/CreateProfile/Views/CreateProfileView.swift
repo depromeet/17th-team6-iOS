faedb1afb5ecf5784b7e76006d7fe803b8161a6f
Jaehui Yu <pkj0522@naver.com>
2025-11-08 03:10:01 +0900

    feat: 프로필 이미지 다운샘플링 기능 추가 (메모리 사용량 최적화)

--- DoRunDoRun/Sources/Features/SignupFlow/CreateProfile/Views/CreateProfileView.swift
+++ DoRunDoRun/Sources/Features/SignupFlow/CreateProfile/Views/CreateProfileView.swift
@@ -91,9 +91,12 @@ extension CreateProfileView {
         .onChange(of: selectedItem) { newItem in
             guard let item = newItem else { return }
             Task {
-                if let data = try? await item.loadTransferable(type: Data.self),
-                   let image = UIImage(data: data) {
-                    store.send(.imagePicked(image))
+                if let data = try? await item.loadTransferable(type: Data.self) {
+                    // 프로필용 권장 크기 (ex: 300x300pt)
+                    let targetSize = CGSize(width: 300, height: 300)
+                    if let downsampledImage = ImageDownsampler.downsample(imageData: data, to: targetSize) {
+                        store.send(.imagePicked(downsampledImage))
+                    }
                 }
             }
         }
